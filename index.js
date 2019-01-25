17e4d61fd9def3abff533342111d0c4c17887e92
davidthkoeln <david.buglowski@smail.th-koeln.de>
2019-01-25 18:11:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var fs = require("fs");
 var content = fs.readFileSync("festivals.json");
-const jsonFestivals = JSON.parse(content);
+var json = JSON.parse(content);
 
 app.use(express.json());
 
@@ -14,32 +14,40 @@ app.get('/',(req,res) => {
 });
 
 app.get('/festivals', (req,res) => {
-    res.json(jsonFestivals);
+    res.json(json.response.festivals);
 
 });
-
+    
 app.get('/festivals/:festivalid',(req,res) => {
     //const festival = jsonFestivals.id.find(f => f.id === parseInt(req.params.festivalid));
     //if (!festival) res.status(404).send('Ein Festival mit dieser ID konnte nicht gefunden werden.')
 
     res.send(
         '<h3>Willkommen auf der Seite des Festivals:</h3>'
-        + '<h2>' + jsonFestivals[req.params.festivalid-1].name + '</h2>' 
+        + '<h2>' + json.response.festivals[req.params.festivalid-1].name + '</h2>' 
         + '<p>Genre:'
-        + jsonFestivals[req.params.festivalid-1].genre + '<p>'
+        + json.response.festivals[req.params.festivalid-1].genre + '<p>'
+        + '<p>Ort:'
+        + json.response.festivals[req.params.festivalid-1].ort + '<p>'
         + '<br>Beschreibung:<br>'
-        + '<p>' + jsonFestivals[req.params.festivalid-1].info + '<p>'  
-        +'<br><form action="http://localhost:3000" method="GET"><button>Home</button>'
-        );
+        + '<p>' + json.response.festivals[req.params.festivalid-1].info + '<p>'  
+        +'<br><form action="http://localhost:3000" method="GET"><button>Home</button>');
 });
 
 app.post('festivals/:festivalid/bewertungen',(req,res) => {
-
+    
 });
-
-app.get('festivals/:genre',(req,res) => {
-
-});
+app.get('/festivals/:genre', (req,res) => {
+    var ergebnis = [];
+          var result = json.response.festivals.filter(function(item) {
+            return item.genre === req.params.genre //Change 3 to what you want to search for
+          });
+          
+          for(i = 0; i < result.length; i++){
+              ergebnis.push(result[i].name);
+          }
+          res.send(ergebnis)
+    });
 
 app.get('festivals/:ort',(req,res) => {
 
